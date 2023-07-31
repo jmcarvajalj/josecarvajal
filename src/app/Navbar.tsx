41c54ce52bc58fb154d113388fcaf949886e70f4
Jose Miguel Carvajal Jimenez <josemcj07@gmail.com>
2023-07-31 11:49:48 -0500

    added navigation and route components

---
+++ src/app/Navbar.tsx
@@ -0,0 +1,59 @@
+'use client';
+import Link from "next/link";
+import { usePathname } from 'next/navigation';
+import { useEffect } from "react";
+
+
+export default function Navbar() {
+    const currentRoute = usePathname();
+
+    // styles for active and non-active links
+    const activeStyle = "active";
+    const nonActiveStyle = "non-active";
+
+    return (
+        <header>
+            {/* language links*/}
+            <nav id="navbar-languages">
+                <Link
+                    href={(currentRoute === "/en" || currentRoute === "/") ? "/" : "/proyectos"}
+                    className={
+                        (currentRoute === "/" || currentRoute === "/proyectos") ? activeStyle : nonActiveStyle
+                    }
+                >
+                    Español
+                </Link>
+                <Link
+                    href={(currentRoute === "/en" || currentRoute === "/") ? "/en" : "/en/projects"}
+                    className={
+                        (currentRoute === "/en" || currentRoute === "/en/projects") ? activeStyle : nonActiveStyle
+                    }
+                >
+                    English
+                </Link>
+            </nav>
+            {/* language links*/}
+
+            <nav id="nombre">José Miguel Carvajal Jiménez</nav>
+
+
+            {/* navigation links*/}
+            <nav id="navbar-links">
+                <Link
+                    href={(currentRoute === "/" || currentRoute === "/proyectos") ? "/" : "/en"}
+                    className={(currentRoute === "/" || currentRoute === "/en") ? activeStyle : nonActiveStyle}
+                >
+                    {(currentRoute === "/" || currentRoute === "/proyectos") ? "Inicio" : "Home"}
+                </Link>
+
+                <Link
+                    href={(currentRoute === "/" || currentRoute === "/proyectos") ? "/proyectos" : "/en/projects"}
+                    className={(currentRoute === "/proyectos" || currentRoute === "/en/projects") ? activeStyle : nonActiveStyle}
+                >
+                    {(currentRoute === "/" || currentRoute === "/proyectos") ? "Proyectos" : "Projects"}
+                </Link>
+            </nav>
+            {/* End navigation menu */}
+        </header >
+    )
+}
