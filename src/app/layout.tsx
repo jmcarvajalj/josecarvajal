38861bbfa2da01c43969366c165ee1ca387bb3aa
Jose Miguel Carvajal Jimenez <josemcj07@gmail.com>
2024-09-24 11:09:19 -0500

    optimized fonts loading speed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,22 @@
-import Navbar from './Navbar'
-import './globals.css'
-import { Montserrat } from 'next/font/google'
+import Navbar from './Navbar';
+import './globals.css';
+import { Montserrat } from 'next/font/google';
 
-const montserrat = Montserrat({ subsets: ['latin'] })
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  display: 'swap'
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      </head>
       <body className={montserrat.className}>
         <Navbar />
         <div className="wave"></div>
@@ -19,5 +25,5 @@ export default function RootLayout({
         {children}
       </body>
     </html>
-  )
+  );
 }
